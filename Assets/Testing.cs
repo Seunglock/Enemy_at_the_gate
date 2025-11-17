1e46a7b4600024510a277159210a4ad4b19186b9
ludiki1122 <162287248+ludiki1122@users.noreply.github.com>
2025-11-17 15:15:39 +0900

    Update Testing.cs
    yoon testing

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -7,7 +7,7 @@ public class Testing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello");
+        print("Hello+testing");
     }
 
     // Update is called once per frame
