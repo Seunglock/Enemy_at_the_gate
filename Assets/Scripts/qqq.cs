0ed19853ec5707fc3d577ae94966fb8f64619f37
Hyojun02 <105540161+Hyojun02@users.noreply.github.com>
2025-11-17 15:37:34 +0900

    Update qqq.cs

--- Assets/Scripts/qqq.cs
+++ Assets/Scripts/qqq.cs
@@ -7,7 +7,7 @@ public class qqq : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Asdf");
     }
 
     // Update is called once per frame
