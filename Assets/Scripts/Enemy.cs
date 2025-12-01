ec7242c879ece1d1f33a59e1d08b721153b845e2
Hyojun02 <105540161+Hyojun02@users.noreply.github.com>
2025-12-01 15:58:57 +0900

    Monsters Add
    dd

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,36 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     private Animator anim;
+
     public float hp = 5f;
     public int goldReward = 5;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 2f;
 
     private float maxHp;
     private float originalSpeed;
     private float currentSpeed;
 
+    private Transform[] path;
+    private int index = 0;
+
+    private Vector3 originalScale;
 
     void Start()
     {
-        originalSpeed = moveSpeed; // Enemy에서 쓰는 이동 속도 변수
+        originalSpeed = moveSpeed;
         currentSpeed = moveSpeed;
         maxHp = hp;
 
         anim = GetComponent<Animator>();
+
+        originalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (path == null || index >= path.Length)
+            return;
+
+        Transform target = path[index];
+
+        if (target.position.x < transform.position.x)
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        else
+            transform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target.position,
+            currentSpeed * Time.deltaTime
+        );
+
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            index++;
+
+            if (index >= path.Length)
+            {
+                ReachGoal();
+            }
+        }
+    }
+
+    void ReachGoal()
+    {
+        Destroy(gameObject);
     }
 
     public void ApplySlow(float slowPercent, float duration)
     {
-        StopAllCoroutines();  // 슬로우 중첩 방지
+        StopAllCoroutines();
         StartCoroutine(SlowEffect(slowPercent, duration));
     }
 
-    IEnumerator SlowEffect(float slowPercent, float duration)
+    System.Collections.IEnumerator SlowEffect(float slowPercent, float duration)
     {
         currentSpeed = originalSpeed * (1f - slowPercent);
         yield return new WaitForSeconds(duration);
@@ -40,31 +83,31 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float damage)
     {
         hp -= damage;
-        Debug.Log("Enemy health: " + name +":"+ hp);
+
         if (hp <= 0)
-        {
             Die();
-        }
     }
 
     public void TakePercentDamage(float percent)
     {
         float damageAmount = maxHp * percent;
         hp -= damageAmount;
-        Debug.Log("Enemy health: " + name + ":" + hp);
-        if (hp <= 0)
-        {
-            Die();
-        }
-    }
 
+        if (hp <= 0)
+            Die();
+    }
 
     void Die()
     {
         if (SystemController.instance != null)
-        {
             SystemController.instance.AddGold(goldReward);
-        }
+
         Destroy(gameObject);
+    }
+
+    public void SetPath(Transform[] newPath)
+    {
+        path = newPath;
+        index = 0;
     }
 }
